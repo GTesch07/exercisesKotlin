27305572be1083b1302d6888ddad869b483ebfe0
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-07-02 20:37:00 -0300

    Desafio: Criar uma Array com 5 números inteiros e exibir o maior deles.

---
+++ lista.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val numeros = arrayOf(1, 2, 3, 4, 5)
+    // A função maxOrNull() encontra o maior elemento na array.
+
+    val maiorNumero = numeros.maxOrNull()
+    println("O maior número é: $maiorNumero")
+
+
+}
