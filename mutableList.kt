c55a717bfc981d31bce2f6941925a4f856358915
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-07-02 21:44:02 -0300

    Alterando mutableList.

--- mutableList.kt
+++ mutableList.kt
@@ -1,6 +1,21 @@
 fun main(){
-    val listaMutavel = mutableListOf("Banana", "Maça", "Laranja", "Melancia")
-    listaMutavel.add("Abacaxi")
-    println(listaMutavel)
+   
+    // 1. Cria uma lista mutável com algumas frutas iniciais.
+    val frutas = mutableListOf("Banana", "Maçã", "Laranja", "Melancia")
+    println("Lista de frutas inicial: $frutas")
 
+    // 2. Pede ao usuário para digitar uma nova fruta.
+    println("\nDigite o nome de uma fruta para adicionar à lista:")
+    val novaFruta = readLine()
+
+    // 3. Adiciona a fruta à lista se a entrada não for nula ou vazia.
+    // A verificação !.isNullOrBlank() é uma boa prática para evitar adicionar entradas vazias.
+    
+    if (!novaFruta.isNullOrBlank()) {
+        frutas.add(novaFruta)
+        println("\n'${novaFruta}' foi adicionada com sucesso!")
+        println("Lista de frutas atualizada: $frutas")
+    } else {
+        println("\nNenhuma fruta foi adicionada.")
+    }
 }
